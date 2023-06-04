02360a8412a91833a9f5647f09e5717ffaaf3c8d
gyubinheo <gjrbqls95@naver.com>
2023-06-05 01:48:19 +0900

    ADD: shop apps, shop models

--- config/settings.py
+++ config/settings.py
@@ -31,12 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # djagno apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third apps
+    "rest_framework",
+    "drf_yasg",
+    # local apps
+    "shop",
 ]
 
 MIDDLEWARE = [
